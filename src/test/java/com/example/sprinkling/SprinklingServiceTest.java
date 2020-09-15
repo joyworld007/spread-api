6dc98c8ea8fc94ae6c6d5204404f5c2096583010
kane <kane@gccompany.co.kr>
2020-09-15 14:26:47 +0900

    Add test case

--- src/test/java/com/example/sprinkling/SprinklingServiceTest.java
+++ src/test/java/com/example/sprinkling/SprinklingServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.sprinkling;
 
 import com.example.sprinkling.service.SprinklingServiceImpl;
+import java.util.Arrays;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,13 +19,18 @@ public class SprinklingServiceTest {
   @InjectMocks
   private SprinklingServiceImpl sprinklingService;
 
-  @DisplayName("test")
+  @DisplayName("random money sum test")
   @Test
-  public void test() {
-    long result[] = sprinklingService.calculationSprinkling(5, 10000);
-    int i = 0;
-    for (long value : result) {
-      System.out.println(i + " : " + result[i++]);
-    }
+  public void readom_money_sum_test() {
+    int money = 10000;
+    long result[];
+    result = sprinklingService.calculationSprinkling(2, money);
+    Assertions.assertEquals(money, Arrays.stream(result).sum());
+    result = sprinklingService.calculationSprinkling(3, money);
+    Assertions.assertEquals(money, Arrays.stream(result).sum());
+    result = sprinklingService.calculationSprinkling(4, money);
+    Assertions.assertEquals(money, Arrays.stream(result).sum());
+    result = sprinklingService.calculationSprinkling(5, money);
+    Assertions.assertEquals(money, Arrays.stream(result).sum());
   }
 }
